9c1d1c4a5f9cd354398818b74e3e1ded928dce73
Nicolas Nalepa <42wmqshjh6@privaterelay.appleid.com>
2026-09-13 15:41:40 -0400

    Define the sequence analysis interface

---
+++ dna_analysis.h
@@ -0,0 +1,5 @@
+float calculate_gc_content(const char *dna_sequence);
+int find_motif(const char *dna_sequence, const char *motif);
+int calculate_genetic_difference(const char *seq1, const char *seq2);
+int is_palindrome(const char *dna_sequence);
+int are_anagrams(const char *seq1, const char *seq2);
